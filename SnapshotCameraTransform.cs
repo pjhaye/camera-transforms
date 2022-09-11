94e92574e14382f64f223030a7d8b8d5b6ce3712
pjhaye <pj.haye@gmail.com>
2022-09-11 12:11:15 -0700

    Initial commit

---
+++ SnapshotCameraTransform.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace CameraTransforms
+{
+    /// <summary>
+    /// Simple assignable CameraTransform values
+    /// </summary>
+    public struct SnapshotCameraTransform : ICameraTransform
+    {
+        public float FieldOfView { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3 Forward { get; set; }
+        public Vector3 Right { get; set; }
+        public Quaternion Rotation { get; set; }
+    }
+}
