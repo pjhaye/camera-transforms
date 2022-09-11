7b11dcded297f7171fd8bcfb3188e4910228f2d3
PJ Haye <pj@tictocgames.com>
2022-09-11 12:20:51 -0700

    Add DOTWEEN block around DG include

--- BlendGameCameraOnEnable.cs
+++ BlendGameCameraOnEnable.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using DG.Tweening;
 using UnityEngine;
+#if CAMERA_TRANSFORMS_DOTWEEN
+using DG.Tweening;
+#endif
 
 namespace CameraTransforms
 {
